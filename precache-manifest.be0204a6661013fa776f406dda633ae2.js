1a9da0226904c2348c0f72d74755608a1d93d5ec
Otacílio Siqueira <38449763+otaciliohsiqueira@users.noreply.github.com>
2019-09-21 17:06:12 -0300

    Desculpe estou com preguiça

---
+++ precache-manifest.be0204a6661013fa776f406dda633ae2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf53b15d774292e538ae6ec9fa991cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf94e65f770e97b97a3f",
+    "url": "/static/css/main.f3b2ebd1.chunk.css"
+  },
+  {
+    "revision": "3597b40b4a8f0c367cab",
+    "url": "/static/js/2.ad1facc1.chunk.js"
+  },
+  {
+    "revision": "cf94e65f770e97b97a3f",
+    "url": "/static/js/main.af89fd2d.chunk.js"
+  },
+  {
+    "revision": "b52b7aef7dee69daa79d",
+    "url": "/static/js/runtime-main.b80357ed.js"
+  }
+]);
